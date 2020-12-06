78a697293c3bb77b5c4bd8e045681d48b0a6897e
Ilan Prais <ilandovprais@gmail.com>
2020-12-06 17:46:12 +0200

    finished traces

--- vfiles/v4/client.py
+++ vfiles/v4/client.py
@@ -3,7 +3,7 @@ import socket,sys
 TCP_IP = sys.argv[1]
 TCP_PORT = int(sys.argv[2])
 BUFFER_SIZE = 1024
-MESSAGE = "Hello, World!"
+MESSAGE = b"Hello, World!"
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((TCP_IP, TCP_PORT))
@@ -14,5 +14,5 @@ s.send(MESSAGE*10)
 data = s.recv(BUFFER_SIZE)
 s.close()
 
-print "received data:", data
+print("received data:", data)
 
