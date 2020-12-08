bc007586ff106cc2e688d3d5c8a65a237bf9a73b
Ilan Prais <ilandovprais@gmail.com>
2020-12-08 20:10:07 +0200

    fixes

--- server.py
+++ server.py
@@ -11,7 +11,7 @@ class server:
 
     def open(self, clientHandler):
         #opening the server socket that will listen to messages from the client
-        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) 
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
         #binding the socket to the server port
         s.bind(('', self.__port))
         #defining the backlog of the socket
@@ -31,58 +31,59 @@ class clienthandler:
         is_client_connected = True
         while is_client_connected:
             #getting the message from the client
-            message = sock.recv(self.__buffer_size)
+            data = client_socket.recv(self.__buffer_size)
             message = data.decode()
 
             #getting the file name
-            tmp = message.find(' ') + 1
-            file_name = message[tmp:message.find(' ', tmp)]
+            file_name = message.split("HTTP")[1].split("1.1")[0]
             if file_name == '/':
                 file_name = 'index.html'
 
             #getting the connection status
-            ‫‪tmp = message.find('Connection: ') + 12
-            connection_status = message[tmp:message.find(' ', tmp)]
+            tmp = message.find('Connection: ') + len('Connection: ')
+            connection_status = message[tmp:message.find('\r\n', tmp)]
 
             #getting the correct log
             if file_name == '‫‪/redirect‬‬':
-                log = create_log(404, 'Not Found', 'close', ' ')
+                log = clientHandler.create_log(301, '‫‪Moved‬‬ ‫‪Permanently‬‬', 'close', '')
                 is_client_connected = False
             try:
                 open_mode = 'rb' if file_name.endswith('ico') or file_name.endswith('jpeg') else 'r'
-                f = open(file_name, 'r')
-                log = create_log(200, 'OK', connection, f.read())
+                f = open(file_name, open_mode)
+                log = clientHandler.create_log(200, 'OK', connection_status, f.read())
             except FileNotFoundError:
-                log = create_log(301, '‫‪Moved‬‬ ‫‪Permanently‬‬', 'close', ' ')
+                log = clientHandler.create_log(404, 'Not Found', 'close', '')
                 is_client_connected = False
 
-            client_socket.send(log)
+            print(log)
 
-            if connection_status = 'close':
+            client_socket.send(log.encode())
+
+            if connection_status == 'close':
                 is_client_connected = False
+
 
         client_socket.close()
 
     @staticmethod
     def create_log(error_code, status, connection_status, content):
 
-        log = '‫‪HTTP/1.1‬‬ ' + str(error_code) + ' ' + status + '\n' + 'Connection: ' + connection_status + '\n'
+        log = "HTTP/1.1‬‬ " + str(error_code) + ' ' + status + '\r\n' + "Connection: " + connection_status + "\r\n"
 
         if error_code == 301:
-            log += '‫‪Location:‬‬ ‫‪/result.html‬‬\n'
+            log += '‫‪Location:‬‬ ‫‪/result.html‬‬\r\n'
         if content:
-            log += '‫‪Content-Length:‬‬ ' + str(content.length())
+            log += '‫‪Content-Length:‬‬ ' + str(len(content))
 
-        log += '\n\n'
+        log += '\r\n\r\n'
 
-        if content:
+        if len(content) > 0:
             log += content
 
         return log
 
-
 #creating the server and the client handler with the command line arguments
 server = server(int(sys.argv[1]))
-clientHandler = clienthandler(filehandler(sys.argv[4]), sys.argv[2], int(sys.argv[3]))
+clientHandler = clienthandler()
 #opening the server with the client handler
 server.open(clientHandler)
